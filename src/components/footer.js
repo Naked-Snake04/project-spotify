0880df8d499af0bc60f20e00381e98e2fe10bc9b
Naked-Snake04 <joKer041200saV@gmail.com>
2022-06-09 00:53:55 +0300

    Исправление header и добавление Footer

---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <footer class="footer">
+        <p>© Basov inc. 2022</p>
+        <nav class="footer__navigation">
+          <a class="link" href="/">О нас</a>
+          <a class="link" href="/">Насчёт....</a>
+          <a class="link"
+          href="https://github.com/Naked-Snake04"
+          target="_blank"
+          rel="noopener">
+        Github создателя</a>
+        </nav>
+    </footer>
+    );
+}
+
+export default Footer; 
