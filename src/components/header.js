5dc155329119c71a5941c593dd6b7e0fbb6f9184
Naked-Snake04 <joKer041200saV@gmail.com>
2022-06-17 19:55:59 +0300

    Исправление

--- src/components/header.js
+++ src/components/header.js
@@ -2,23 +2,6 @@ import React from "react";
 import Input from "./Input";
 
 function Header() {
-  // const { setSearchData } = useContext(Context);
-  // const ERR = 'Данные по запросу не найдены';
-  
-  // function getResult(value) {
-  //   if (value !== "") {
-  //     Request.getApiData(
-  //       `https://ws.audioscrobbler.com/2.0/?method=track.search&limit=100&track=${value}&api_key=a15921d44a0fcd14ba611ad341dbf3d6&format=json`
-  //     ).then((data) => {
-  //       if (typeof data === 'undefined') {
-  //         console.log(ERR);
-  //       }
-  //       else {
-  //         return setSearchData(data.results.trackmatches.track);
-  //       }
-  //     });
-  //   } else setSearchData([]);
-  // }
   return (
     <header className="header">
       <a href="/" className="header__logo">
